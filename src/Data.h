a09dab3576d90588875339ca1bc6b609f2b73e34
Beatriz de Henriques Martins <up201502858@fe.up.pt>
2016-11-20 22:47:11 +0000

    documentação

--- src/Data.h
+++ src/Data.h
@@ -20,22 +20,76 @@ class Data {
 	int ano;
 
 public:
+	/**
+	 * @brief construtor da classe Data vazio
+	 */
 	Data();
+	/**
+	 * @brief construtor da classe Data com parametros
+	 * @param d - dia
+	 * @param m - mes
+	 * @param a - ano
+	 */
 	Data(int d, int m, int a);
+
+	/**
+	 * @brief função para obter dia
+	 * @return dia
+	 */
 	int getDia() const;
+
+	/**
+	 * @brief função para obter mes
+	 * @return mes
+	 */
 	int getMes() const;
+
+	/**
+	 * @brief função para obter ano
+	 * @return ano
+	 */
 	int getAno() const;
+
+	/**
+	 * @brief função que actualiza dia
+	 * @param d- dia
+	 */
 	void setDia(int d);
+
+	/**
+	 * @brief função que actualiza mes
+	 * @param m - mes
+	 */
 	void setMes(int m);
+
+	/**
+	 * @brief função que actualiza ano
+	 * @param a - ano
+	 */
 	void setAno(int a);
+
+	/**
+	 * @brief função para imprimir data com formatação especifica
+	 * @return string data
+	 */
 	string toString();
+
+	/**
+	 * @brief função operador<< impressão para data com formatação especifica
+	 * @param os - ostream
+	 * @param d -data
+	 * @return string data
+	 */
 	friend ostream & operator <<(ostream os, Data d);
 };
 
 class DataInvalida {
 public:
+	/**
+	 * @brief função classe excessão
+	 */
 	void dataErrada() {
-		cout << "Data invalida"<<endl;
+		cout << "Data invalida" << endl;
 	}
 
 };
